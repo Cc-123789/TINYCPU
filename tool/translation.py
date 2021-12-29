d00bc7ee704929b45f3a21753d2c82a8ec1c0404
吴忠恒 <2402252516@qq.com>
2021-12-29 12:14:13 +0800

    完善指令翻译程序;1.增加了寄存器别名处理 2.修复了一些指令的翻译异常

--- tool/translation.py
+++ tool/translation.py
@@ -40,10 +40,14 @@ class Translation():
 
         # 加载指令机器码对应json文件
         with open('op.json','r',encoding='utf-8') as f:
-            self.op_dict = json.load(f)
+            self.__op_dict = json.load(f)
+
+        # 加载寄存器别名json文件
+        with open('reg.json','r',encoding='utf-8') as f:
+            self.__reg_dict = json.load(f)
 
         # 初始化指令对应的翻译方法字典
-        self.fun_dict = {
+        self.__fun_dict = {
             'r':self._R_translation,
             'i':self._I_translation,
             'j':self._J_translation,
@@ -51,6 +55,14 @@ class Translation():
             'regimm':self._REGIMM_translation,
             'lb':self._LB_translation
         }
+
+        self.__addr = 0
+
+        self.__tempfile = "temp.txt"
+
+    # MIPS汇编预编译方法，f_code为代码文件的文件操作符
+    def pre_translation(self,instruction):
+        pass                                                           # 有点麻烦，暂时保留       
 
     #指令翻译方法,具有很弱的代码格式化能力
     def translation(self,instruction):
@@ -70,13 +82,13 @@ class Translation():
             return ''
 
         try :
-            instruction_form = self.op_dict[ segements[0] ]                #指令操作符对应格式
+            instruction_form = self.__op_dict[ segements[0] ]                #指令操作符对应格式
         except KeyError:
             print('不存在该关键字',segements[0])                            #如果有异常，则助记符写错了
             return
 
         #调用指令对应格式的翻译方法
-        result = self.fun_dict[ instruction_form['ins_kind'] ](segements[1:],instruction_form)     
+        result = self.__fun_dict[ instruction_form['ins_kind'] ](segements[1:],instruction_form)     
 
         return result
 
@@ -87,11 +99,17 @@ class Translation():
         result = ''
         for flag in zeros_reg:
             if flag == '0':
-                string = re.sub(r'\$t', r'', segements[i])           # 去掉$t
-                result = result + "{:0>5b}".format( int(string) )    # 把标号转为2进制,并加入
-                i = i + 1
+                try:
+                    string = re.sub(r'\$', r'', segements[i])            # 去掉$
+                    if not string.isdecimal():                           # 如果寄存器名为别名，进行处理
+                        string = self.__reg_dict[string]
+                    result = result + "{:0>5b}".format( int(string) )    # 把标号转为2进制,并加入
+                    i = i + 1
+                except IndexError:
+                    print("该指令寄存器数量异常")
+                    exit(-1)
             else:
-                result = result + "00000"                            # 标志为1则为零号寄存器
+                result = result + "00000"                             # 标志为1则为零号寄存器
         return result
 
     #指令对应格式的翻译方法如下
@@ -163,12 +181,22 @@ class Translation():
 
     def _LB_translation(self,segements,instruction_form):
 
-        result = instruction_form['op']  +  segements[0]    # 加入操作码和基址
+        result = instruction_form['op']     # 加入操作码
 
         # 寄存器译码
-        result = result + self._reg_translation(segements,instruction_form['zeros_reg']) 
+        reg = self._reg_translation(segements,instruction_form['zeros_reg']) 
+
+        # 基址寄存器译码
+        base = re.search(r'\(.*\)',segements[-1]).group()                       # 寻找基址寄存器
+        base = re.sub(r'\((.*)\)',r'\1',base)                                   # 去掉小括号
+        base = self._reg_translation([base],"0")                                # 基址寄存器译码
+        result = result + base
+
+        # 加入寄存器
+        result = result + reg
 
         #加入偏移量
+        segements[-1] = re.sub(r'\(.*\)',r'',segements[-1] )
         result = result + "{:0>16b}".format( int( segements[-1] ) ) 
 
         return result
@@ -177,6 +205,7 @@ if __name__ == '__main__':
     opener = Opener()                                        #初始化打开文件对象(通过GUI操作)
     file_path = opener.opentext_by_gui()                     #调用文件文档打开方法
     translation = Translation()                              #初始化指令翻译对象
+    save_path = ''
     if file_path:                                            #如果文件打开成功
         with open(file_path,'r',encoding='utf-8') as f:      #以只读方式打开，请保证文件是UTF-8编码
             save_path = opener.savetext_by_gui()
@@ -185,9 +214,13 @@ if __name__ == '__main__':
                     if line != '':                               #读取到有效行
                         temp = translation.translation(line)     #指令译码
                         if temp:                                 #有效译码
+                            print(temp)
                             temp = re.findall(r'.{4}',temp)      #按四个二进制数切割，准备转换为十六进制
-
                             # 二进制转为十六进制
                             temp = [ "{:x}".format( int(binary,2) ) for binary in temp ] 
                             temp = ''.join(temp)                 #列表拼接为字符串
                             w.write(temp+'\n')                   #写入文件
+    else:
+        print("文件被占用或无效")
+        exit(-1)
+    print( "文件成功保存为" + save_path )
