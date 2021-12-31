9ae72b15e267fde1ebc17be90ec362b0decb25f9
曹小羽 <1210875605@qq.com>
2021-12-31 10:42:09 +0800

    啦啦啦

--- core/stage/ex/EX.v
+++ core/stage/ex/EX.v
@@ -114,6 +114,56 @@ module EX(
     end
   end
 
+
+/** 函数pos：确定操作数有效位数   ***/
+function [4:0] pos;
+  input [31:0] op_div_1;
+  reg [15:0] sel1;
+  reg [ 7:0] sel2;
+  reg [ 3:0] sel3;
+  reg [ 2:0] sel4;
+  begin
+	if(|op_div_1[31:16] == 0) begin
+		pos[4] = 0;
+		sel1 = op_div_1[15:0];
+		end
+	else begin
+		pos[4] = 1;
+		sel1 = op_div_1[31:16];
+	end
+	if(|sel1[15:8] == 1'b0) begin
+		pos[3] = 0;
+		sel2 = sel1[7:0];
+	end
+	else begin
+		pos[3] = 1;
+		sel2 = sel1[15:8];
+	end
+	if(|sel2[7:4] == 1'b0) begin
+		pos[2] = 0;
+		sel3 = sel2[3:0];
+	end
+	else begin
+		pos[2] = 1;
+		sel3 = sel2[7:4];
+	end
+	if(|sel3[3:2] == 1'b0) begin
+		pos[1] = 0;
+		sel4 = sel3[1:0];
+	end
+	else begin
+		pos[1] = 1;
+		sel4 = sel3[3:2];
+	end
+	if(|sel4[1] == 1'b0) begin
+		pos[0] = 0;                             
+	end
+	else begin
+		pos[0] = 1;
+	end
+ end
+endfunction
+
     /*****************
     ** DIV/DIVU   ***
     ******************/
@@ -122,7 +172,8 @@ module EX(
   wire [`DATA_BUS] div_quo = 0;//商，初值为0
   wire [`DATA_BUS] div_rem = op_div_1;//余数，初值为被除数
   wire [`DATA_BUS] div_temp = 0;//暂存中间结果
-  
+  wire[`REG_ADDR_BUS] n_1;
+  wire[`REG_ADDR_BUS] n_2;
   //被除数：若为有符号乘法且该乘数为负数，则取其补码，否则不变
   wire[`DATA_BUS] op_div_1 = 
           (funct == `FUNCT_DIV && operand_1[31])?
@@ -131,17 +182,10 @@ module EX(
   wire[`DATA_BUS] op_div_2 = 
           (funct == `FUNCT_DIV && operand_2[31])?
           op2_c : operand_2;
+          
+  assign n_1 = pos(op_div_1);
+  assign n_2 = pos(op_div_2);
 
-  always @(*) begin
-    wire[`REG_ADDR_BUS] n_1 = 32'h11111111;
-    wire[`REG_ADDR_BUS] n_2 = 32'h11111111;
-    while (!op_div_1[n_1]) begin
-      n_1 = n_1-1;
-    end
-    while (!op_div_2[n_2]) begin
-      n_2 = n_2-1;
-    end
-  end
 
   div_shift_cnt = n_1 - n_2;
 
@@ -228,5 +272,7 @@ always @(*) begin
       default: result <= 0;
     endcase
   end
+
+
 
 endmodule // EX
