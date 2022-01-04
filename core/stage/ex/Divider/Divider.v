59315914dc09abd4235ac36e2d1dc15c81166cf1
吴忠恒 <2402252516@qq.com>
2022-01-04 08:48:08 +0800

    修复除法器商和余数的异常(未测试)

--- core/stage/ex/Divider/Divider.v
+++ core/stage/ex/Divider/Divider.v
@@ -9,55 +9,73 @@ module Divider (
   output      [`DOUBLE_DATA_BUS]  result
 );
 
-wire[`DATA_BUS]                     op1[`DATA_BUS_WIDTH:0];
-wire[`DATA_BUS]                     op2[`DATA_BUS_WIDTH:0];
-wire[`DATA_BUS_WIDTH:0]             cout;
-wire[`DATA_BUS_WIDTH:0]             add_sub_en_link;
+wire[`DATA_BUS]                     op1[`DATA_BUS];
+wire[`DATA_BUS]                     op2[`DATA_BUS];
+wire[`DATA_BUS]                     cout;
+wire[`DATA_BUS]                     add_sub_en_link;
 wire[`DATA_BUS]                     quo;
-wire[`DATA_BUS]                     rem[`DATA_BUS_WIDTH:0];
-wire[`DATA_BUS]                     temp_op1;
-wire[`DATA_BUS]                     temp_op2;
+wire[`DATA_BUS]                     rem[`DATA_BUS];
 wire[`DATA_BUS]                     temp_quo;
 wire[`DATA_BUS]                     temp_rem;
 
-assign temp_op1 = operand_1[`DATA_BUS_WIDTH-1] ? ~operand_1 + 1 : operand_1;
-assign temp_op2 = operand_2[`DATA_BUS_WIDTH-1] ? ~operand_2 + 1 : operand_2;
-assign op2[0] = temp_op2;
-assign add_sub_en_link[0] = ~ ( temp_op1[`DATA_BUS_WIDTH-1] ^ temp_op2[`DATA_BUS_WIDTH-1] );
-assign op1[0] = { `DATA_BUS_WIDTH{ temp_op1[`DATA_BUS_WIDTH-1] } };
 
-genvar i,j;
+assign op2[0] = op2;
+assign add_sub_en_link[0] = ~ ( operand_1[`DATA_BUS_WIDTH-1] ^ operand_2[`DATA_BUS_WIDTH-1] );
+assign op1[0] = { `DATA_BUS_WIDTH{ operand_1[`DATA_BUS_WIDTH-1] } };
 
-for ( i = 1; i<= `DATA_BUS_WIDTH; i = i + 1 ) begin
+genvar i;
+
+for ( i = 1; i< `DATA_BUS_WIDTH; i = i + 1 ) begin
   assign add_sub_en_link[i] = ~ ( rem[i-1][`DATA_BUS_WIDTH-1] ^ op2[i][`DATA_BUS_WIDTH-1] );
 end
 
 for ( i = 1; i < `DATA_BUS_WIDTH; i = i + 1) begin
-  assign op1[i][`DATA_BUS_WIDTH-1:1] = rem[i-1][`DATA_BUS_WIDTH-2:0];
-  assign op1[i][0] = temp_op1[`DATA_BUS_WIDTH-1-i];     
+  assign begin
+    op1[i][`DATA_BUS_WIDTH-1:1] = rem[i-1][`DATA_BUS_WIDTH-2:0];
+    op1[i][0] = operand_1[`DATA_BUS_WIDTH-1-i];   
+  end
 end
-
-assign op1[`DATA_BUS_WIDTH] = rem[`DATA_BUS_WIDTH-1];
 
 for (i=0;i < `DATA_BUS_WIDTH; i = i + 1) begin
   assign quo[i] = cout[`DATA_BUS_WIDTH-1-i] ^ add_sub_en_link[`DATA_BUS_WIDTH-1-i];  
 end
 
-for( i=0; i <= `DATA_BUS_WIDTH ; i=i+1) begin
+for( i=0; i < `DATA_BUS_WIDTH ; i=i+1) begin
     cas_row u_cas_row(
-		.op1( op1[i] ),
-		.op2( op2[i] ),
-    .add_sub_en_in( add_sub_en_link[i] ),
-    .cout( cout[i] ),
-    .divisor( op2[i+1] ),
-    .rem( rem[i] )
+      .op1( op1[i] ),
+      .op2( op2[i] ),
+      .add_sub_en_in( add_sub_en_link[i] ),
+      .cout( cout[i] ),
+      .divisor( op2[i+1] ),
+      .rem( rem[i] )
     );
 end
 
-//assign temp_rem = quo[0] ? rem[`DATA_BUS_WIDTH] + temp_op2 : rem[`DATA_BUS_WIDTH];
-assign temp_quo = ( operand_1[`DATA_BUS_WIDTH-1] ^ operand_2[`DATA_BUS_WIDTH-1]) ? ~quo + 1 : quo;
-assign temp_rem =   operand_1[`DATA_BUS_WIDTH-1] ? ~rem[`DATA_BUS_WIDTH] + 1 : rem[`DATA_BUS_WIDTH];
+//修正余数：如果余数和被除数同号，不需要修正，否则，如果被除数和除数同号，余数加上被除数，否则余数减去被除数。
+assign begin
+  if ( rem[`DATA_BUS_WIDTH-1][`DATA_BUS_WIDTH-1] ^  operand_1[`DATA_BUS_WIDTH-1]) begin
+    temp_rem = rem[`DATA_BUS_WIDTH-1];
+  end
+  else begin
+    if ( operand_1[`DATA_BUS_WIDTH-1] ^ operand_2[`DATA_BUS_WIDTH-1] ) begin
+      temp_rem = rem[`DATA_BUS_WIDTH-1] - operand_2;   
+    end
+    else begin
+      temp_rem = rem[`DATA_BUS_WIDTH-1] + operand_2;       
+    end 
+  end
+end
 
-assign result = { temp_rem,temp_quo };
+//修正商：如果被除数和除数异号，商加一。
+assign begin
+    if ( operand_1[`DATA_BUS_WIDTH-1] ^ operand_2[`DATA_BUS_WIDTH-1] ) begin
+      temp_quo = quo + 1;   
+    end
+    else begin
+      temp_quo = quo;       
+    end 
+end
+
+assign result = { temp_rem,temp_rem };
 
 endmodule
--- core/stage/ex/Divider/cas_row.v
+++ core/stage/ex/Divider/cas_row.v
@@ -22,12 +22,12 @@ genvar i;
 
 for( i=0; i<`DATA_BUS_WIDTH; i=i+1) begin
     cas u_cas(
-    .add_sub_en_in ( add_sub_en_link[i] ),
-    .cin (temp[i]),
-    .op1 (op1[i]),
-    .op2 (op2[i]),
-    .result (rem[i]),
-    .cout (temp[i+1])
+        .add_sub_en_in ( add_sub_en_link[i] ),
+        .cin (temp[i]),
+        .op1 (op1[i]),
+        .op2 (op2[i]),
+        .result (rem[i]),
+        .cout (temp[i+1])
     );
 end
 
